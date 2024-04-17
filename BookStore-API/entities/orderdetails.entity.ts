bb4f85fdc579e7c3ee623ba0a654944cec30d520
Ngoc Anh <ngocanh30927@gmail.com>
2024-04-17 17:12:07 +0700

    up

--- BookStore-API/entities/orderdetails.entity.ts
+++ BookStore-API/entities/orderdetails.entity.ts
@@ -1,13 +1,19 @@
-import { Check, Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Check, Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 import { IsNotEmpty, MaxLength, validateOrReject } from 'class-validator';
 import { Book } from './book.entity';
 import { Order } from './orders.entity';
 
 @Entity({ name: 'OrderDetails' })
-@Check(`"Discount" >= 0`, `"Discount" <= 100`)
+@Check(`"Discount" >= 0`)
+@Check(`"Discount" <= 100`)
 export class OrderDetail {
-    @PrimaryGeneratedColumn({ name: 'Id' })
-    id: number;
+   
+    @PrimaryColumn({ type: 'int' })
+    bookId: number;
+
+    @PrimaryColumn({ type: 'int' })
+    orderId: number;
+
 
     @IsNotEmpty()
     @Column({ name: 'Quantity', type: 'decimal', precision: 18, scale: 2, default: 1 })
@@ -18,23 +24,17 @@ export class OrderDetail {
     price: number;
 
     @IsNotEmpty()
-    @Column({ name: 'Discount', type: 'decimal', precision: 18, scale: 2 })
+    @Column({ name: 'Discount', type: 'decimal', precision: 18, scale: 2})
     discount: number;
 
     @IsNotEmpty()
     @Column({ name: 'SubTotalOrder', type: 'money' })
     subtotalorder: number;
 
-    @Column({ type: 'int' })
-    bookId: number;
+   
 
-    @Column({ type: 'int' })
-    orderId: number;
-
-
-    @ManyToMany(() => Book)
-    @JoinTable()
-    books: Book[];
+    @ManyToOne(() => Book, (b) => b.orderdetails)
+    book: Book;
 
     @ManyToOne(() => Order, (o) => o.orderDetails)
     order: Order;
